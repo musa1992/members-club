8d792f253b08d96ed60a4390f64b0bf0c19a61fb
musa <mosesbwire91@gmail.com>
2021-04-14 21:36:29 +0300

    scaffold the post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :posts
   devise_for :users
   devise_scope :user do
     unauthenticated do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_163951) do
+ActiveRecord::Schema.define(version: 2021_04_14_182809) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "post"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
