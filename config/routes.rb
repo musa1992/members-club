8582fdf511d8f6494dd9baa15d6db56bb346ab73
musa <mosesbwire91@gmail.com>
2021-04-14 21:57:33 +0300

    scaffold comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   
-  resources :posts
+  
   devise_for :users
   devise_scope :user do
+    authenticated :user do
+      root 'posts#index', as: :authenticated_root
+    end
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
+  end
+  resources :posts do
+    resources :comments
   end
 end
